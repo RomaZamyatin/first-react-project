4b3867088db0e4576a1210b09bebe7b510a6eaaf
Roman Zamyatin <Romikz12@mail.ru>
2026-01-29 15:17:42 +0300

    rewrote app.jsx without using jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,31 @@
-//декларативный стиль, так как тут используем готовые решения, реализация не важна
-import { useState } from 'react'
+import { createElement, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
-  //декларативный
+
   const [count, setCount] = useState(0)
   let date = new Date();
-  // вся верстка - декларативный стиль
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-		{/*Кроме этого, тут императивный - описываем как работает счётчик*/}
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more <br></br>
-		{date.getFullYear()} 
-      </p>
-    </>
+	createElement(React.Fragment,null,
+		createElement('div',null,
+			createElement('a', {href: 'https://vite.dev',target:'_blank'},
+				createElement('img',{src:viteLogo,className:'logo',alt:'Vite Logo'})
+			),
+			createElement('a', {href: 'https://react.dev',target:'_blank'},
+				createElement('img',{src:reactLogo,className:'logo react',alt:'React Logo'})
+			)
+		),
+		createElement('h1',null,'Vite + React'),
+		createElement('div',{className:'card'},
+			createElement('button',{onClick:()=>setCount(count+1)},`count is ${count}`),
+			createElement('p',null,`edit `,createElement('code',null,'src/App.jsx'), ` and save to test HMR`),
+		),
+		createElement('p',{className:"read-the-docs"},'Click on the Vite and React logos to learn more'),
+		createElement('p',null,`${date.getFullYear()}`)
+	)
   )
 }
-//декларативный
 export default App
